64231258fa659ca18f48f3816a194c27dd29cf01
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-08-01 23:04:22 +0600

    completed

--- contest-2/Tree_Plant.cpp
+++ contest-2/Tree_Plant.cpp
@@ -3,8 +3,10 @@ using namespace std;
 int main(){
     long long int x;cin>>x;
     if(x==0)cout<<0;
-    if(x==1)cout<<1;
-    long long int result=(pow(2,x-2)*pow(2,x-1));
-    cout<<result;
+    else if(x==1)cout<<1;
+    else{
+        long long int result=(pow(2,x-2)*pow(2,x-1));
+        cout<<result;
+    }
     return 0;
 }
