d04fef6aedb297d257cd201ac464bac9488bc593
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-10-11 17:25:38 +0600

    mission accomplished 🔥🚀🔥

--- VJudge/10_October_2023/F_Substrings_Sort.cpp
+++ VJudge/10_October_2023/F_Substrings_Sort.cpp
@@ -4,17 +4,6 @@ using namespace std;
 typedef pair<ll,ll>pii;
 #define forl(var,str,end) for(long long int var=str; var<end; var++)
 int main(){
-    int n;cin>>n;
-    set<string>st;
-    vector<string>v;
-    while(n--){
-        string s;cin>>s;
-        st.insert(s);
-    }
-    while(!st.empty()){
-        v.push_back(*st.begin());
-        st.erase(st.begin());
-    }
-    for(int i=0;i<v.size();i++)cout<<v[i]<<endl;
+    
     return 0;
 }
