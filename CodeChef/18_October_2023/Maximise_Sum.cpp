674de5cc67096199123fdb31cc4231df366aa73b
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-10-21 11:23:55 +0600

    mission accomplished 🔥🚀🔥

--- CodeChef/18_October_2023/Maximise_Sum.cpp
+++ CodeChef/18_October_2023/Maximise_Sum.cpp
@@ -17,8 +17,16 @@ void solve(){
     ll n;cin>>n;
     ll a[n],sum=0;
     for(ll i=0;i<n;i++)cin>>a[i];
-    sort(a,a+n);
-    for(ll i=0;i<n-1;i++)a[i]=a[n-2];
+    ll pre[n],post[n];
+    pre[0]=a[0],post[n-1]=a[n-1];
+    for(ll i=1;i<n;i++)pre[i]=max(a[i],pre[i-1]);
+    for(ll i=n-2;i>=0;i--)post[i]=max(a[i],post[i+1]);
+    for(ll i=1;i<n-1;i++){
+        if(a[i]>=pre[i-1]||a[i]>=post[i+1])continue;
+        else {
+            a[i]=min(pre[i-1],post[i+1]);
+        }
+    }
     for(ll i=0;i<n;i++)sum+=a[i];
     c(sum);
 }
