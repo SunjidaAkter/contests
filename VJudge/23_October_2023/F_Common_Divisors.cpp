e7b818789379c2f05d3c101a10314fa5706af5bb
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-10-24 22:51:46 +0600

    mission accomplished 🔥🚀🔥

--- VJudge/23_October_2023/F_Common_Divisors.cpp
+++ VJudge/23_October_2023/F_Common_Divisors.cpp
@@ -4,13 +4,21 @@ using namespace std;
 typedef pair<ll,ll>pii;
 #define forl(var,str,end) for(long long int var=str; var<end; var++)
 int main(){
-    int n;cin>>n;
-    int a[n];
-    for(int i=0;i<n;i++)cin>>a[i];
-    sort(a,a+n);
-    vector<int>div;
-    for(int i=1;i<=a[0];i++){
-        if(a[0]%i==0)div.push_back(i);
+    ll n;cin>>n;
+    ll a[n],sum=0;
+    for(ll i=0;i<n;i++)cin>>a[i];
+    ll gcd=a[0];
+    for(ll i=1;i<n;i++){
+        gcd=__gcd(gcd,a[i]);
     }
+    sort(a,a+n);
+    vector<ll>div;
+    for(ll i=1;i*i<=gcd;i++){
+        if(gcd%i==0){
+            if(gcd/i==i)sum++;
+            else sum+=2;
+        }
+    }
+    cout<<sum<<endl;
     return 0;
 }
