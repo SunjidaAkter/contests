15b6f590baf89e672e35f5acb28d9011f8dddd7f
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-11-06 20:36:56 +0600

    mission accomplished 🔥🚀🔥

---
+++ VJudge/06_November_2023/A_A.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+typedef pair<ll,ll>pii;
+#define forl(var,str,end) for(long long int var=str; var<end; var++)
+int main(){
+    int x,m,n;cin>>x>>m>>n;
+    if((x+n)>=m)cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+    return 0;
+}
