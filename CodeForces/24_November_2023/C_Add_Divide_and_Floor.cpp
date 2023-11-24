df774c450f0dd80d76f35609dafb0bf7d7ac9d6e
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-11-24 22:45:34 +0600

    mission accomplished 🔥🚀🔥

--- CodeForces/24_November_2023/C_Add_Divide_and_Floor.cpp
+++ CodeForces/24_November_2023/C_Add_Divide_and_Floor.cpp
@@ -1,47 +1,49 @@
-#include <bits/stdc++.h>
-#include <iostream>
-#include <vector>
-
+#include<bits/stdc++.h>
 using namespace std;
 #define ll long long
-
-pair<int, vector<ll>> min_operations(int n, vector<ll>& a) {
-    if (unordered_set<ll>(a.begin(), a.end()).size() == 1) {
-        return {0, {}};
+#define all(X) (X).begin(), (X).end()
+#define pub push_back
+#define pob pop_back
+#define nl  '\n'
+#define c(x) cout<<x<<nl
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+typedef pair<int,int>pii;
+#define forl(ty,var,str,end) for(ty var=str; var<end; var++)
+# define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+void solve(){
+    int n;cin>>n;
+    double a[n];
+    set<int>st;
+    for(int i=0;i<n;i++){cin>>a[i];st.insert(a[i]);}
+    if(n==1||st.size()==1)cout<<0<<nl;
+    else{
+        int cnt=0;
+        while(st.size()>1){
+            st.clear();
+            ll sum=0;
+            for(int i=1;i<n;i++){
+                sum+=(a[i-1]-a[i]);
+            }
+            ll avg=sum/(n-1);
+            for(int i=1;i<n;i++){
+                a[i]=floor((a[i]+avg)/2);
+                cout<<a[i]<<' ';
+                st.insert(a[i]);
+            }
+            cout<<nl;
+            cnt++;
+        }
+        cout<<cnt<<nl;
     }
-
-    vector<ll> x_values;
-    for (int i = 1; i < n; ++i) {
-        ll x = (a[i] - a[0]) / i;
-        x_values.push_back(x);
-    }
-
-    return {static_cast<int>(x_values.size()), x_values};
 }
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-
-        vector<ll> a(n);
-        for (int i = 0; i < n; ++i) {
-            cin >> a[i];
-        }
-
-        auto [ops, x_values] = min_operations(n, a);
-
-        cout << ops << "\n";
-        for (auto x : x_values) {
-            cout << x << " ";
-        }
-        cout << "\n";
+int main(){
+    FAST;
+    int t;cin>>t;
+    while(t--){
+        solve();
     }
-
     return 0;
 }
