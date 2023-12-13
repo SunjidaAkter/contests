75f9bce90fe3ebd5a6f672435c2e6c19a38f17b0
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-12-13 17:18:41 +0600

    mission accomplished 🔥🚀🔥

--- CodeChef/6_December_2023/Prefixing.cpp
+++ CodeChef/6_December_2023/Prefixing.cpp
@@ -1,41 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-void solve() {
-    int n;
-    cin >> n;
-
-    vector<int> a(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> a[i];
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define pub push_back
+#define pob pop_back
+#define nl  '\n'
+#define c(x) cout<<x<<nl
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+typedef pair<int,int>pii;
+#define forl(ty,var,str,end) for(ty var=str; var<end; var++)
+# define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+void solve(){
+    ll n,mx=0;cin>>n;
+    vector<ll>a(n);
+    vector<ll>v(n,0);
+    map<ll,ll>mp;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        mp[a[i]]++;
     }
-
-    set<int> st;
-    vector<int> b;
-
-    for (int i = n - 1; i >= 0; --i) {
-        if (!st.count(a[i])) {
-            b.push_back(a[i]);
-            st.insert(a[i]);
+    for(int i=0;i<n;i++){
+        mx=max(mx,a[i]);
+    }
+    for(int i=0;i<n;i++){
+        if(mp[a[i]]!=0){
+            v[i]=a[i];
+            mp[a[i]]=0;
         }
     }
-
-    reverse(b.begin(), b.end());
-
-    for (int i = 0; i < b.size(); ++i) {
-        cout << b[i] << " ";
+    for(int i=0;i<n;i++){
+        if(v[i]==0){
+            v[i]=mx;
+        }
     }
-
-    cout << endl;
+    for(ll val:v)cout<<val<<" ";
+    cout<<nl;
 }
-
-int main() {
-    int t;
-    cin >> t;
-
-    while (t--) {
+int main(){
+    FAST;
+    int t;cin>>t;
+    while(t--){
         solve();
     }
-
     return 0;
 }
