1009d4c584da1cf3bd2f634b9e7ae5ea5a4048f6
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-01-27 22:51:13 +0600

    mission accomplished 🔥🚀🔥

---
+++ CodeForces/01-27-2024_Div-2/A_We_Got_Everything_Covered.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    int n,k; cin>>n>>k;
+    string s;
+    char c='a';
+    for(int i=1;i<=k;i++){
+        s += c;
+        c++;
+    }
+    string q=s;
+    for(int i=1;i<n;i++){
+        s += q;
+    }
+    cout << s << endl;
+}
+int main(){
+    FAST;
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
---
+++ CodeForces/01-27-2024_Div-2/B_A_Balanced_Problemset.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll n,k;cin>>n>>k;
+    ll gcd=1;
+for (int i = 1; i <= n; i++)
+    {
+        if (!(n% i) && n/ i >= k)
+                gcd = i;
+           
+        if (n / i < k)
+            break;
+    }
+    cout<<gcd<<nl;
+}
+int main(){
+    FAST;
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
