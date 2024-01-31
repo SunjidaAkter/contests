6647bce2ede12689556fdfb7e756b10ce06e5647
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-01-31 22:36:56 +0600

    mission accomplished 🔥🚀🔥

---
+++ CodeChef/01_31_2024/A.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll a[5];
+    for(ll i=0;i<5;i++)cin>>a[i];
+    ll cnt=0;
+    for(ll i=0;i<5;i++){
+        if(a[i]==1)cnt++;
+    }
+    if(cnt>=4)yes;
+    else no;
+}
+int main(){
+    FAST;
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
