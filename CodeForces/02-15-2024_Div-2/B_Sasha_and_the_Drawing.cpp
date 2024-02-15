cc818b2fdda0456405c33214f8ad8800bb0122a2
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-02-15 22:41:06 +0600

    mission accomplished 🔥🚀🔥

---
+++ CodeForces/02-15-2024_Div-2/B_Sasha_and_the_Drawing.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll n,k;cin>>n>>k;
+    if(k==(4*n-2)){
+        cout<<2*n<<nl;
+        return;
+    }
+    if(k==(4*n-3)){
+        cout<<(2*n)-1<<nl;
+        return;
+    }
+    cout<<(k/2)+(k%2)<<nl;
+}
+int main(){
+    FAST;
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
